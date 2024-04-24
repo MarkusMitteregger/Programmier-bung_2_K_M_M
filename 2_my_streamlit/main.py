ffa09867b44aaf9852d23d3d9b5396a078f0a9dd
annamauthner <ma4455@mci4me.at>
2024-04-24 15:26:48 +0200

    Zu viele Bilder

--- 2_my_streamlit/main.py
+++ 2_my_streamlit/main.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import read_data # Ergänzen Ihr eigenes Modul
 
+# Eine Überschrift der ersten Ebene
+st.write("# EKG APP")
+
+# Eine Überschrift der zweiten Ebene
+st.write("## Versuchsperson auswählen")
+
 # Legen Sie eine neue Liste mit den Personennamen an indem Sie ihre 
 # Funktionen aufrufen
 person_dict = read_data.load_person_data()
@@ -12,3 +18,51 @@ person_names = read_data.get_person_list(person_dict)
 st.session_state.current_user = st.selectbox(
     'Versuchsperson',
     options = person_names, key="sbVersuchsperson")
+
+
+
+from PIL import Image
+# Laden eines Bilds
+image = Image.open("../data/pictures/js.jpg")
+# Anzeigen eines Bilds mit Caption
+st.image(image, caption=st.session_state.current_user)
+
+
+
+def find_person_data_by_name(suchstring):
+    """ Eine Funktion der Nachname, Vorname als ein String übergeben wird
+    und die die Person als Dictionary zurück gibt"""
+
+    person_data = load_person_data()
+    #print(suchstring)
+    if suchstring == "None":
+        return {}
+
+    two_names = suchstring.split(", ")
+    vorname = two_names[1]
+    nachname = two_names[0]
+
+    for eintrag in person_data:
+        print(eintrag)
+        if (eintrag["lastname"] == nachname and eintrag["firstname"] == vorname):
+            print()
+
+            return eintrag
+    else:
+        return {}
+    
+    # Anlegen des Session State. Bild, wenn es kein Bild gibt
+if 'picture_path' not in st.session_state:
+    st.session_state.picture_path = 'data/pictures/none.jpg'
+
+# ...
+
+# Suche den Pfad zum Bild, aber nur wenn der Name bekannt ist
+if st.session_state.current_user in person_names:
+    st.session_state.picture_path = read_data.find_person_data_by_name(st.session_state.current_user)["picture_path"]
+
+# ...
+
+# Öffne das Bild und Zeige es an
+image = Image.open("../" + st.session_state.picture_path)
+st.image(image, caption=st.session_state.current_user)
--- 2_my_streamlit/read_data.py
+++ 2_my_streamlit/read_data.py
@@ -61,3 +61,18 @@ def find_person_data_by_name(suchstring):
     else:
         return {}
     
+
+    
+suchstring  = "Huber, Julian"
+
+# Teilt einen String in und speichert die Ergebnisse in einer Liste
+two_names = suchstring.split(", ")
+vorname = two_names[1]
+nachname = two_names[0]
+
+person_data = load_person_data()
+
+# Nun können wir vergleichen bis wir einen Treffer finden
+for eintrag in person_data:
+    if (eintrag["lastname"] == nachname and eintrag["firstname"] == vorname):
+        print(eintrag)
