17b1697674a27407d5cce8adb83c184cb92d86c8
annamauthner <ma4455@mci4me.at>
2024-04-24 14:54:12 +0200

    Auswahlliste

--- 2_my_streamlit/main.py
+++ 2_my_streamlit/main.py
@@ -1,15 +1,14 @@
 import streamlit as st
+import read_data # Ergänzen Ihr eigenes Modul
 
-st.write("# EKG APP")
-st.write("## Versuchsperson auswählen")
+# Legen Sie eine neue Liste mit den Personennamen an indem Sie ihre 
+# Funktionen aufrufen
+person_dict = read_data.load_person_data()
+person_names = read_data.get_person_list(person_dict)
+# bzw: wenn Sie nicht zwei separate Funktionen haben
+# person_names = read_data.get_person_list()
 
-# Session State wird leer angelegt, solange er noch nicht existiert
-if 'current_user' not in st.session_state:
-    st.session_state.current_user = 'None'
-
-# Dieses Mal speichern wir die Auswahl als Session State
+# Nutzen Sie ihre neue Liste anstelle der hard-gecodeten Lösung
 st.session_state.current_user = st.selectbox(
     'Versuchsperson',
-    options = ["Nutzer1", "Nutzer2"], key="sbVersuchsperson")
-
-st.write("Der Name ist: ", st.session_state.current_user) 
+    options = person_names, key="sbVersuchsperson")
--- 2_my_streamlit/read_data.py
+++ 2_my_streamlit/read_data.py
@@ -1,9 +1,24 @@
 import json
 
 # Opening JSON file
-file = open("data/person_db.json")
+file = open("../data/person_db.json")
 
 # Loading the JSON File in a dictionary
 person_data = json.load(file)
 
 person_data
+
+def load_person_data():
+    """A Function that knows where the person database is and returns a dictionary with the persons"""
+    file = open("../data/person_db.json")
+    person_data = json.load(file)
+    return person_data
+
+def get_person_list(person_data):
+    """A Function that takes the persons-dictionary and returns a list auf all person names"""
+    list_of_names = []
+
+    for eintrag in person_data:
+        list_of_names.append(eintrag["lastname"] + ", " +  eintrag["firstname"])
+    return list_of_names
+
