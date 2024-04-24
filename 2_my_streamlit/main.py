c445155ed7cd3175319d78621fe21c467303bc11
ekrepper <e.krepper@mci4me.at>
2024-04-24 15:29:47 +0200

    Bilder gefixt

--- 2_my_streamlit/main.py
+++ 2_my_streamlit/main.py
@@ -1,5 +1,6 @@
 import streamlit as st
-import read_data # Ergänzen Ihr eigenes Modul
+import read_data
+from PIL import Image
 
 # Eine Überschrift der ersten Ebene
 st.write("# EKG APP")
@@ -11,58 +12,24 @@ st.write("## Versuchsperson auswählen")
 # Funktionen aufrufen
 person_dict = read_data.load_person_data()
 person_names = read_data.get_person_list(person_dict)
-# bzw: wenn Sie nicht zwei separate Funktionen haben
-# person_names = read_data.get_person_list()
 
 # Nutzen Sie ihre neue Liste anstelle der hard-gecodeten Lösung
 st.session_state.current_user = st.selectbox(
     'Versuchsperson',
-    options = person_names, key="sbVersuchsperson")
+    options=person_names, key="sbVersuchsperson"
+)
 
-
-
-from PIL import Image
-# Laden eines Bilds
-image = Image.open("../data/pictures/js.jpg")
-# Anzeigen eines Bilds mit Caption
-st.image(image, caption=st.session_state.current_user)
-
-
-
-def find_person_data_by_name(suchstring):
-    """ Eine Funktion der Nachname, Vorname als ein String übergeben wird
-    und die die Person als Dictionary zurück gibt"""
-
-    person_data = load_person_data()
-    #print(suchstring)
-    if suchstring == "None":
-        return {}
-
-    two_names = suchstring.split(", ")
-    vorname = two_names[1]
-    nachname = two_names[0]
-
-    for eintrag in person_data:
-        print(eintrag)
-        if (eintrag["lastname"] == nachname and eintrag["firstname"] == vorname):
-            print()
-
-            return eintrag
+# Search for the image path only if the name is known
+if st.session_state.current_user in person_names:
+    person_data = read_data.find_person_data_by_name(st.session_state.current_user)
+    if person_data:
+        st.session_state.picture_path = person_data["picture_path"]
     else:
-        return {}
-    
-    # Anlegen des Session State. Bild, wenn es kein Bild gibt
-if 'picture_path' not in st.session_state:
+        st.session_state.picture_path = 'data/pictures/none.jpg'
+else:
     st.session_state.picture_path = 'data/pictures/none.jpg'
 
-# ...
-
-# Suche den Pfad zum Bild, aber nur wenn der Name bekannt ist
-if st.session_state.current_user in person_names:
-    st.session_state.picture_path = read_data.find_person_data_by_name(st.session_state.current_user)["picture_path"]
-
-# ...
-
-# Öffne das Bild und Zeige es an
-image = Image.open("../" + st.session_state.picture_path)
+# Open the image and display it
+image_path = "../" + st.session_state.picture_path
+image = Image.open(image_path)
 st.image(image, caption=st.session_state.current_user)
