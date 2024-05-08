f8f4a21a6905e9425756d0385b906e67832fb300
Ihr Name auf github <Ihre E-Mail von github>
2024-05-08 11:14:02 +0200

    Streamlit_App Überarbeitet

--- 2_my_streamlit/read_data.py
+++ 2_my_streamlit/read_data.py
@@ -1,4 +1,3 @@
-User
 import json
 
 def load_person_data():
@@ -32,46 +31,7 @@ def find_person_data_by_name(suchstring):
     for eintrag in person_data:
         print(eintrag)
         if (eintrag["lastname"] == nachname and eintrag["firstname"] == vorname):
-            print()
 
             return eintrag
     else:
         return {}
-
-
-if __name__ == "__main__":
-    # Opening JSON file
-    file = open("../data/person_db.json")
-
-    # Loading the JSON File in a dictionary
-    person_data = json.load(file)
-
-    person_data
-
-    suchstring  = "Huber, Julian"
-
-    # Teilt einen String in und speichert die Ergebnisse in einer Liste
-    two_names = suchstring.split(", ")
-    vorname = two_names[1]
-    nachname = two_names[0]
-
-    person_data = load_person_data()
-
-    # Nun können wir vergleichen bis wir einen Treffer finden
-    for eintrag in person_data:
-        if (eintrag["lastname"] == nachname and eintrag["firstname"] == vorname):
-            print(eintrag)
-            
-    suchstring  = "Huber, Julian"
-
-    # Teilt einen String in und speichert die Ergebnisse in einer Liste
-    two_names = suchstring.split(", ")
-    vorname = two_names[1]
-    nachname = two_names[0]
-
-    person_data = load_person_data()
-
-    # Nun können wir vergleichen bis wir einen Treffer finden
-    for eintrag in person_data:
-        if (eintrag["lastname"] == nachname and eintrag["firstname"] == vorname):
-            print(eintrag)
