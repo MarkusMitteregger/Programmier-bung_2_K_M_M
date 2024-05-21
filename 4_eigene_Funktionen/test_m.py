657ebe678e169f85c10a1d326d082be8e0ddfc06
ekrepper <e.krepper@mci4me.at>
2024-05-21 22:34:53 +0200

    Powercurve Plot fertig mit Marker :)

--- 4_eigene_Funktionen/test_m.py
+++ 4_eigene_Funktionen/test_m.py
@@ -52,7 +52,7 @@ def calc_powercurve(df):
     df_clean = df.dropna(subset = "PowerOriginal")
     array_best_effort = []
     array_time_window = []
-    for window in range(1200):
+    for window in range(1201):
         value = best_effort(df_clean, window)
         array_best_effort.append(value)
         array_time_window.append(window)
