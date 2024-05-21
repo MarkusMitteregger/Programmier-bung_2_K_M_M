9f407b0cfb358fde858797d7fa5b3685c71d88c5
Ihr Name auf github <Ihre E-Mail von github>
2024-05-21 13:48:39 +0200

    Find Peaks drinne

--- 4_eigene_Funktionen/test_m.py
+++ 4_eigene_Funktionen/test_m.py
@@ -4,8 +4,37 @@ import matplotlib.pyplot as plt
 
 
 def find_peaks(ekg_data):
-    print("noch nicht gemacht")
+    def find_peaks(series, threshold, respacing_factor=5):
+    """
+    A function to find the peaks in a series
+    Args:
+        - series (pd.Series): The series to find the peaks in
+        - threshold (float): The threshold for the peaks
+        - respacing_factor (int): The factor to respace the series
+    Returns:
+        - peaks (list): A list of the indices of the peaks
+    """
+    # Respace the series
+    series = series.iloc[::respacing_factor]
+    
+    # Filter the series
+    series = series[series>threshold]
 
+
+    peaks = []
+    last = 0
+    current = 0
+    next = 0
+
+    for index, row in series.items():
+        last = current
+        current = next
+        next = row
+
+        if last < current and current > next and current > threshold:
+            peaks.append(index-respacing_factor)
+
+    return peaks
 
 
 #Funktion zum einlesen der EKG-Daten
