60d03ca88c577b6012f167b1a2129447a99f20c9
ekrepper <e.krepper@mci4me.at>
2024-06-01 16:40:55 +0200

    nah dran

--- 5_zusammenfuehrung/Patienten-Datenbank.py
+++ 5_zusammenfuehrung/Patienten-Datenbank.py
@@ -2,13 +2,16 @@ import streamlit as st
 import read_data # Ergänzen Sie Ihr eigenes Modul
 from PIL import Image
 import test_a as a
-import test_m as m 
+import test_m as m
 
 # Eine Überschrift der ersten Ebene
 st.write("# PATIENTEN-DATENBANK")
 
-# Legen Sie eine neue Liste mit den Personennamen an indem Sie ihre 
-patients = a.Person.get_person_list(person_data=a.Person.load_person_data())
+# Laden Sie die Personendaten
+person_data = a.Person.load_person_data()
+
+# Legen Sie eine neue Liste mit den Personennamen an
+patients = a.Person.get_person_list(person_data=person_data)
 
 # Fügen Sie einen Platzhalter für die Dropdown-Liste hinzu
 patients.insert(0, "Wählen Sie einen Patienten aus")
@@ -26,11 +29,28 @@ if selected_patient != "Wählen Sie einen Patienten aus":
     # Suche den Pfad zum Bild, aber nur wenn der Name bekannt ist
     st.session_state.picture_path = read_data.find_person_data_by_name(st.session_state.current_user)["picture_path"]
 else:
-    st.session_state.picture_path = 'data/pictures/Patientendatenbank.jpg' 
+    st.session_state.picture_path = 'data/pictures/Patientendatenbank.jpg'
     st.session_state.current_user = ""
 
-# Öffne das Bild und Zeige es an
+# Öffne das Bild und zeige es an
 image = Image.open(st.session_state.picture_path)
 st.image(image, caption=st.session_state.current_user if 'current_user' in st.session_state else "")
 
-
+# Prüfen, ob ein Patient ausgewählt wurde und EKG-Tests laden
+if selected_patient != "Wählen Sie einen Patienten aus":
+    patient_data = read_data.find_person_data_by_name(selected_patient)
+    ekg_tests = patient_data.get("ekg_tests", [])
+    ekg_options = ["Wählen Sie einen Test aus"] + [f"Test-ID {ekg['id']}; Datum: {ekg['date']}" for ekg in ekg_tests]
+    
+    selected_ekg = st.selectbox("Wählen Sie einen EKG-Test aus", options=ekg_options)
+    
+    if selected_ekg != "Wählen Sie einen Test aus":
+        # Extrahiere die EKG-ID sicher
+        ekg_id = int(selected_ekg.split(" ")[1].replace(";", ""))
+        ekg_data = m.EKGdata.load_by_id(ekg_id)
+        
+        if ekg_data:
+            st.write(f"Herzfrequenz: {ekg_data.heartrate:.2f} bpm")
+            ekg_data.make_plot()
+        else:
+            st.write("Keine EKG-Daten gefunden.")
