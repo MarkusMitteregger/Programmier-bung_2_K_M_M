48a0019a478db61f01f4e75aa1184bd396cf4f4e
ekrepper <e.krepper@mci4me.at>
2024-06-02 17:07:07 +0200

    zusammen leistungstest powercurve und ekg

--- 5_zusammenfuehrung/Patienten-Datenbank.py
+++ 5_zusammenfuehrung/Patienten-Datenbank.py
@@ -4,6 +4,7 @@ import test_a as a
 import test_m as m
 import pandas as pd
 import plotly.graph_objects as go
+import plotly.express as px
 
 # Eine Überschrift der ersten Ebene
 st.write("# PATIENTEN-DATENBANK")
@@ -12,9 +13,7 @@ st.write("# PATIENTEN-DATENBANK")
 person_data = a.Person.load_person_data()
 
 # Legen Sie eine neue Liste mit den Personennamen an
-patients = a.Person.get_person_list(person_data=person_data)
-
-# Fügen Sie einen Platzhalter für die Dropdown-Liste hinzu
+patients = a.Person.get_person_list(person_data)
 patients.insert(0, "Wählen Sie einen Patienten aus")
 
 # Nutzen Sie ihre neue Liste anstelle der hard-gecodeten Lösung
@@ -27,20 +26,21 @@ if 'picture_path' not in st.session_state:
 # Überprüfen, ob ein tatsächlicher Patient ausgewählt wurde
 if selected_patient != "Wählen Sie einen Patienten aus":
     st.session_state.current_user = selected_patient
-    # Suche den Pfad zum Bild, aber nur wenn der Name bekannt ist
-    st.session_state.picture_path = a.Person.find_person_data_by_name(st.session_state.current_user)["picture_path"]
+    person_data_dict = a.Person.find_person_data_by_name(st.session_state.current_user)
+    st.session_state.picture_path = person_data_dict["picture_path"]
+    person_instance = a.Person(person_data_dict)
 else:
     st.session_state.picture_path = 'data/pictures/Patientendatenbank.jpg'
     st.session_state.current_user = ""
+    person_instance = None
 
 # Öffne das Bild und zeige es an
 image = Image.open(st.session_state.picture_path)
 st.image(image, caption=st.session_state.current_user if 'current_user' in st.session_state else "")
 
 # Prüfen, ob ein Patient ausgewählt wurde und EKG-Tests laden
-if selected_patient != "Wählen Sie einen Patienten aus":
-    patient_data = a.Person.find_person_data_by_name(selected_patient)
-    ekg_tests = patient_data.get("ekg_tests", [])
+if person_instance:
+    ekg_tests = person_instance.ekg_tests
 
     if ekg_tests:
         ekg_options = ["Wählen Sie einen Test aus"] + [f"Test-ID {ekg['id']}; Datum: {ekg['date']}" for ekg in ekg_tests]
@@ -50,7 +50,6 @@ if selected_patient != "Wählen Sie einen Patienten aus":
     selected_ekg = st.selectbox("Wählen Sie einen EKG-Test aus", options=ekg_options)
 
     if selected_ekg != "Wählen Sie einen Test aus" and selected_ekg != "Noch keine EKG-Daten vorhanden":
-        # Extrahiere die EKG-ID sicher
         ekg_id = int(selected_ekg.split(" ")[1].replace(";", ""))
         ekg_data = m.EKGdata.load_by_id(ekg_id)
 
@@ -60,31 +59,8 @@ if selected_patient != "Wählen Sie einen Patienten aus":
         else:
             st.write("Keine EKG-Daten gefunden.")
 
-    # Anlegen des Session State. Bild, wenn es kein Bild gibt
-    if selected_ekg != "Wählen Sie einen Test aus":
-        st.session_state.current_ekg = selected_ekg
-
-        # Suchen des Ergebnislinks für den ausgewählten EKG-Test
-        ekg_id = int(selected_ekg.split(" ")[1].replace(";", ""))
-        result_link = None
-
-        # Durchlaufen der EKG-Tests des ausgewählten Patienten, um den Ergebnislink zu finden
-        for ekg_test in patient_data.get("ekg_tests", []):
-            if ekg_test["id"] == ekg_id:
-                result_link = ekg_test.get("result_link")
-                break
-
-        if result_link:
-            st.write(f"Link zu den EKG-Daten: {result_link}")
-        else:
-            st.write("Kein Link gefunden.")
-    else:
-        st.write("Kein EKG-Test ausgewählt.")
-
     # Prüfen, ob ein Patient ausgewählt wurde und Leistungstest laden
-if selected_patient != "Wählen Sie einen Patienten aus":
-    patient_data = a.Person.find_person_data_by_name(selected_patient)
-    intervall_tests = patient_data.get("intervall_tests", {})
+    intervall_tests = person_instance.intervall_tests
 
     if intervall_tests:
         intervall_test_option = ["Wählen Sie einen Test aus"] + [f"Test-ID {intervall_tests['id']}; Datum: {intervall_tests['date']}"]
@@ -108,65 +84,139 @@ if selected_patient != "Wählen Sie einen Patienten aus":
             else:
                 return 'Zone5'
 
-        # Funktion zur Analyse der Herzfrequenz
         def analyze_heart_rate(df, max_hr):
             df['HeartRateZone'] = df['HeartRate'].apply(lambda x: get_heart_rate_zone(x, max_hr))
             time_in_zones = df.groupby('HeartRateZone')['Duration'].sum()
             time_in_zones = time_in_zones.apply(lambda x: '{:02}:{:02}'.format(int(x) // 60, int(x) % 60))
             return time_in_zones
 
-        # Funktion zur Analyse der Leistung
         def analyze_performance(df):
             avg_performance_in_zones = df.groupby('HeartRateZone')['PowerOriginal'].mean()
             avg_performance_in_zones = avg_performance_in_zones.round().astype(int)
             return avg_performance_in_zones
 
-
         st.title('Herzfrequenzanalyse')
-        for intervall_test in [intervall_tests]:
-            if intervall_test["id"] == intervall_test_id:
-                dateipfad = intervall_test.get("result_link")
+        if intervall_tests["id"] == intervall_test_id:
+            dateipfad = intervall_tests.get("result_link")
 
+            df = pd.read_csv(dateipfad)
+            max_hr = person_instance.max_hr
+            analyze_button = st.button("Analyse durchführen")
 
-                df = pd.read_csv(dateipfad)
-                max_hr = st.sidebar.number_input("Bitte geben Sie die maximale Herzfrequenz ein:", value=0, step=1)
-                analyze_button = st.sidebar.button("Analyse durchführen")
+            if analyze_button:
+                st.write(max_hr)
+                time_in_zones = analyze_heart_rate(df, max_hr)
+                avg_performance_in_zones = analyze_performance(df)
+                avg_performance_generel = df['PowerOriginal'].mean().round().astype(int)
+                max_performance_generel = df['PowerOriginal'].max().round().astype(int)
 
-                if analyze_button:
-                    time_in_zones = analyze_heart_rate(df, max_hr)
-                    avg_performance_in_zones = analyze_performance(df)
-                    avg_performance_generel = df['PowerOriginal'].mean().round().astype(int)
-                    max_performance_generel = df['PowerOriginal'].max().round().astype(int)
+                st.subheader('Zeit in HF-Zonen (in mm˸ss):')
+                st.write(time_in_zones)
+                st.subheader('Durchschnittliche Leistung in den Herzfrequenzzonen (in Watt):')
+                st.write(avg_performance_in_zones)
+                st.subheader('Durchschnittliche Leistung gesamt (in Watt):')
+                st.write(avg_performance_generel)
+                st.subheader('Maximale Leistung (in Watt):')
+                st.write(max_performance_generel)
 
-                    st.subheader('Zeit in HF-Zonen (in mm˸ss):')
-                    st.write(time_in_zones)
-                    st.subheader('Durchschnittliche Leistung in den Herzfrequenzzonen (in Watt):')
-                    st.write(avg_performance_in_zones)
-                    st.subheader('Durchschnittliche Leistung gesamt (in Watt):')
-                    st.write(avg_performance_generel)
-                    st.subheader('Maximale Leistung (in Watt):')
-                    st.write(max_performance_generel)
+                time = df.index / 60
 
-                    time = df.index / 60
+                fig = go.Figure()
 
-                    fig = go.Figure()
+                fig.add_trace(go.Scatter(x=time, y=df['HeartRate'], mode='lines', name='Heart Rate'))
+                fig.add_trace(go.Scatter(x=time, y=df['PowerOriginal'], mode='lines', name='Power'))
 
-                    # Herzfrequenz und Leistung über die Zeit plotten
-                    fig.add_trace(go.Scatter(x=time, y=df['HeartRate'], mode='lines', name='Heart Rate'))
-                    fig.add_trace(go.Scatter(x=time, y=df['PowerOriginal'], mode='lines', name='Power'))
+                fig.add_hrect(y0=0, y1=0.6*max_hr, fillcolor="lightblue", opacity=0.2, line_width=0)
+                fig.add_hrect(y0=0.6*max_hr, y1=0.7*max_hr, fillcolor="lightgreen", opacity=0.2, line_width=0)
+                fig.add_hrect(y0=0.7*max_hr, y1=0.8*max_hr, fillcolor="yellow", opacity=0.2, line_width=0)
+                fig.add_hrect(y0=0.8*max_hr, y1=0.9*max_hr, fillcolor="orange", opacity=0.2, line_width=0)
+                fig.add_hrect(y0=0.9*max_hr, y1=1.0*max_hr, fillcolor="red", opacity=0.2, line_width=0)
 
-                    # Herzfrequenzzonen als Bereiche hinzufügen
-                    fig.add_hrect(y0=0, y1=0.6*max_hr, fillcolor="lightblue", opacity=0.2, line_width=0)
-                    fig.add_hrect(y0=0.6*max_hr, y1=0.7*max_hr, fillcolor="lightgreen", opacity=0.2, line_width=0)
-                    fig.add_hrect(y0=0.7*max_hr, y1=0.8*max_hr, fillcolor="yellow", opacity=0.2, line_width=0)
-                    fig.add_hrect(y0=0.8*max_hr, y1=0.9*max_hr, fillcolor="orange", opacity=0.2, line_width=0)
-                    fig.add_hrect(y0=0.9*max_hr, y1=1.0*max_hr, fillcolor="red", opacity=0.2, line_width=0)
+                fig.update_layout(
+                    title="Leistung und Herzfrequenz über die Zeit",
+                    xaxis_title='Dauer [min]',
+                    yaxis_title='Herzfrequenz [bpm], Leistung [W]'
+                )
 
-                    fig.update_layout(
-                        title="Leistung und Herzfrequenz über die Zeit",
-                        xaxis_title='Dauer [min]',
-                        yaxis_title='Herzfrequenz [bpm], Leistung [W]'
+                st.plotly_chart(fig)
+            
+            powercurve_button = st.button("Powercurve anzeigen")
+
+            if powercurve_button:
+
+                def activity_read_csv():
+                    df = pd.read_csv(dateipfad)
+                    return df
+
+                def best_effort(df, window):
+                    value = df["PowerOriginal"].rolling(window).mean()
+                    return value.max()
+                
+                def calc_powercurve(df):
+                    df_clean = df.dropna(subset = "PowerOriginal")
+                    array_best_effort = []
+                    array_time_window = []
+                    for window in range(1201):
+                        value = best_effort(df_clean, window)
+                        array_best_effort.append(value)
+                        array_time_window.append(window)
+                    powercurve_df = pd.DataFrame({"Power" : array_best_effort, "Time_Window" : array_time_window})
+                    return powercurve_df
+                
+                # Funktion zur Konvertierung von Sekunden in mm:ss Format
+                def seconds_to_mmss(seconds):
+                    minutes, seconds = divmod(seconds, 60)
+                    return f"{minutes:02d}:{seconds:02d}"
+
+                # Datei-Pfad
+                file_path = ("data/activities/activity.csv")	
+                # Leistungsdaten laden
+                df = pd.read_csv(file_path)
+
+                # Berechnet die Powercurve
+                powercurve = calc_powercurve(df)
+
+                # Zeitfenster in mm:ss Format konvertieren
+                powercurve['Time_Window_mmss'] = powercurve['Time_Window'].apply(seconds_to_mmss)
+
+                # Definieren der gewünschten Zeitpunkte in Sekunden
+                desired_times = [1, 30, 60, 100, 300, 600, 1200]
+
+                # Zeitpunkte in mm:ss Format konvertieren
+                xticks_mmss = [seconds_to_mmss(t) for t in desired_times]
+
+                # Filter powercurve für Marker-Daten
+                marker_data = powercurve[powercurve['Time_Window'].isin(desired_times)]
+
+                st.subheader('Powercurve')
+
+                # Zeitreihenplot erstellen
+                fig = px.line(powercurve, x='Time_Window_mmss', y='Power', title='Evaluation PowerCurve')
+
+                # Hinzufügen von Markern zu den spezifischen Zeitpunkten
+                fig.add_trace(go.Scatter(
+                    x=marker_data['Time_Window_mmss'],
+                    y=marker_data['Power'],
+                    mode='markers',
+                    marker=dict(color='red', size=8),
+                    name='Specific values'
+                ))
+
+                # Setze die x-Achse auf die gewünschten Zeitpunkte
+                fig.update_xaxes(title_text='Duration [mm:ss]', tickvals=xticks_mmss)
+                fig.update_yaxes(title_text='Powercurve [W]')
+
+                # Aktualisiere die Layout-Einstellungen für die x-Achse
+                fig.update_layout(
+                    xaxis=dict(
+                        tickmode='array',
+                        tickvals=xticks_mmss,
+                        #ticktext=xticks_mmss,
+                        tickangle=-45  # Winkel der x-Achsen-Beschriftungen, um Überlappung zu vermeiden
                     )
+                )
 
-                    st.plotly_chart(fig)
+                st.plotly_chart(fig)
+
+                        
 
--- 5_zusammenfuehrung/test_a.py
+++ 5_zusammenfuehrung/test_a.py
@@ -1,88 +1,59 @@
-import streamlit as st
-from PIL import Image
-from datetime import datetime
 import json
+from datetime import datetime
 
 class Person:
+    def __init__(self, person_dict):
+        self.date_of_birth = person_dict["date_of_birth"]
+        self.age = self.calculate_age()
+        self.max_hr = self.calc_max_HR()
+        self.firstname = person_dict["firstname"]
+        self.lastname = person_dict["lastname"]
+        self.picture_path = person_dict["picture_path"]
+        self.id = person_dict["id"]
+        self.ekg_tests = person_dict["ekg_tests"]
+        self.intervall_tests = person_dict.get("intervall_tests", {})
 
     def calculate_age(self):
         today = datetime.today().year
-        age = int(today) - self.date_of_birth
+        age = today - self.date_of_birth
         return age
-    
-    def calc_max_HR (self):
-        """Berechnet die maximale Herzfrequenz einer Person."""
-        max_hr = 220 - self.age
-        return max_hr
-    
-    
+
+    def calc_max_HR(self):
+        """Calculates the maximum heart rate for a person."""
+        return 220 - self.age
+
     @staticmethod
-    def load_person_data(arg=None):
-        """Eine Funktion, die weiß, wo sich die Personendatenbank befindet und ein Dictionary mit den Personen zurückgibt."""
+    def load_person_data():
+        """Loads the person database and returns a dictionary with the persons."""
         with open("data/person_db.json", 'r') as file:
             person_data = json.load(file)
         return person_data
 
     @staticmethod
     def get_person_list(person_data):
-        """Eine Funktion, die das Personendaten-Dictionary nimmt und eine Liste aller Personennamen zurückgibt."""
-        list_of_names = []
-
-        for eintrag in person_data:
-            list_of_names.append(eintrag["lastname"] + ", " +  eintrag["firstname"])
-        return list_of_names
+        """Returns a list of all person names from the person data dictionary."""
+        return [f"{entry['lastname']}, {entry['firstname']}" for entry in person_data]
 
     @staticmethod
-    def find_person_data_by_name(suchstring):
-        """Eine Funktion, der Nachname, Vorname als ein String übergeben wird und die die Person als Dictionary zurückgibt."""
+    def find_person_data_by_name(full_name):
+        """Finds and returns person data by the full name string (lastname, firstname)."""
         person_data = Person.load_person_data()
-        
-        if suchstring == "None":
+
+        if full_name == "None":
             return {}
 
-        two_names = suchstring.split(", ")
-        vorname = two_names[1]
-        nachname = two_names[0]
+        lastname, firstname = full_name.split(", ")
 
-        for eintrag in person_data:
-            if (eintrag["lastname"] == nachname and eintrag["firstname"] == vorname):
-                return eintrag
-        else:
-            return {}
-
-    def __init__(self, person_dict):
-        self.date_of_birth = person_dict["date_of_birth"]
-        self.age = self.calculate_age() 
-        self.max_hr = self.calc_max_HR ()
-        self.firstname = person_dict["firstname"]
-        self.lastname = person_dict["lastname"]
-        self.picture_path = person_dict["picture_path"]
-        self.id = person_dict["id"]
-        self.ekg_tests = person_dict["ekg_tests"]
-        self.intervall_tests = person_dict["intervall_tests"]   
+        for entry in person_data:
+            if entry["lastname"] == lastname and entry["firstname"] == firstname:
+                return entry
+        return {}
 
     @staticmethod
     def load_by_id(person_id):
-        """Instanziiert eine Person anhand der ID und der Datenbank."""
+        """Instantiates a Person object based on the ID from the database."""
         person_data = Person.load_person_data()
         for entry in person_data:
             if entry["id"] == person_id:
                 return Person(entry)
-            else: continue
-
-    def __init__(self, person_dict):
-            self.date_of_birth = person_dict["date_of_birth"]
-            self.age = self.calculate_age() 
-            self.max_hr = self.calc_max_HR ()
-            self.firstname = person_dict["firstname"]
-            self.lastname = person_dict["lastname"]
-            self.picture_path = person_dict["picture_path"]
-            self.id = person_dict["id"]
-            self.ekg_tests = person_dict["ekg_tests"]
-            self.intervall_tests = person_dict["intervall_tests"]
-
-    
-
-
-#P_3 = Person.load_by_id(1)
-#print(Person.get_person_list(person_data=Person.load_person_data()))
+        return None
